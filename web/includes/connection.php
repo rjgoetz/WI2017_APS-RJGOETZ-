e092e090996342f4399693e5e03be5f4ed77b461
rjgoetz <rj@rjgoetz.com>
2017-02-11 14:46:37 -0600

    sql connection test

--- web/includes/connection.php
+++ web/includes/connection.php
@@ -2,10 +2,14 @@
 
 if (getenv('env') === 'production') {
 
+  echo 'ENV';
+
   $dbc = mysqli_connect('us-cdbr-iron-east-04.cleardb.net', 'ba42118f85b34c', 'e4f6631c', 'heroku_a498164999f10b4') or die('Error connecting to MySQL server.');
 
 } else {
 
+  echo 'LOCALHOST';
+
   $dbc = mysqli_connect('localhost', 'root', 'root', 'advproj') or die('Error connecting to MySQL server.');
-  
+
 }
